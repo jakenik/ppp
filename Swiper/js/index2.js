36f8d3ab65d1cd8695ea719cc650d9127d24178e
JKKE <2419308218@qq.com>
2017-06-05 14:57:36 +0800

    改

--- Swiper/js/index2.js
+++ Swiper/js/index2.js
@@ -1,15 +1,19 @@
 //获取元素节点
-var jsDiv = $("#playground");
-var jsSnack = $("#snack");
-var jsFood = $("#food");
-var jsBody = $("#playground");
-var jsScore = $("#score");
+var jsDiv = document.getElementById("playground");
+var jsSnack = document.getElementById("snack");
+var jsFood = document.getElementById("food");
+var jsBody = document.getElementById("playground");
+var jsScore = document.getElementById("score");
 //设置全局变量
 var timer; //创建定时器为全局变量
-var timer1 = setInterval(eat, 10); //检测位置碰撞，并且吃掉食物；
+setInterval(eat, 10); //检测位置碰撞，并且吃掉食物；
 var srr = []; //记录蛇行动的位置
 var num = 0; //记录数组的长度
+var playgroundWidth=jsBody.clientWidth;
+var playgroundheight=jsBody.clientHeight;
 var jsSnackBody; //么米吃掉一个食物，蛇的身体
+var jsFoodWidth=jsFood.clientWidth;
+var snackWidth=snack.clientWidth;
 //左-------》对应的ASCII码值是 37；
 //上-------》对应的ASCII码值是 38；
 //右-------》对应的ASCII码值是 39；
@@ -21,9 +25,14 @@ document.onkeydown = function(e) {
 		//向左移动
 		case 37:
 			clearInterval(timer);
+			if(jsSnack.offsetLeft!=1 || jsSnack.offsetTop!=0){
+				console.log(jsSnack.offsetTop)
 			timer = window.setInterval(runLeft, 10)
-			function runLeft() {
+			}
+
+		function runLeft() {
 				if(jsSnack.offsetLeft > 0) {
+//					console.log(jsSnack.offsetLeft)
 					jsSnack.style.left = jsSnack.offsetLeft - 1 + "px";
 					jsSnack.style.top = jsSnack.offsetTop + "px";
 					//记录小蛇的位置
@@ -32,17 +41,22 @@ document.onkeydown = function(e) {
 				}
 			}
 			break;
+			
 			//向上移动
 		case 38:
 			clearInterval(timer);
+			console.log()
+			if(jsSnack.offsetLeft!=1 || jsSnack.offsetTop!=0){
 			timer = window.setInterval(runTop, 10);
-
+			}
 			function runTop() {
 				if(jsSnack.offsetTop > 0) {
 					jsSnack.style.top = jsSnack.offsetTop - 1 + "px";
 					jsSnack.style.left = jsSnack.offsetLeft + "px";
 					//记录小蛇的位置
+					
 					srr.push([jsSnack.offsetLeft, jsSnack.offsetTop]);
+//					console.log(srr)
 					num++; //记录数组的长度
 				}
 			}
@@ -51,10 +65,11 @@ document.onkeydown = function(e) {
 			//向右移动
 		case 39:
 			clearInterval(timer);
+			if(jsSnack.offsetLeft!=1 || jsSnack.offsetTop!=0){
 			timer = window.setInterval(runRight, 10);
-
+			}
 			function runRight() {
-				if(jsSnack.offsetLeft + jsSnack.offsetWidth <= 450) {
+				if(jsSnack.offsetLeft + jsSnack.offsetWidth <= playgroundWidth) {
 					jsSnack.style.left = jsSnack.offsetLeft + 1 + "px";
 					jsSnack.style.top = jsSnack.offsetTop + "px";
 					//记录小蛇的位置
@@ -66,10 +81,12 @@ document.onkeydown = function(e) {
 			//向下移动    
 		case 40:
 			clearInterval(timer);
+			if(jsSnack.offsetLeft!=1 || jsSnack.offsetTop!=0){
 			timer = window.setInterval(runBottom, 10);
-
+			}
 			function runBottom() {
-				if(jsSnack.offsetTop + jsSnack.offsetHeight <= 400) {
+				if(jsSnack.offsetTop + jsSnack.offsetHeight <= playgroundheight) {
+//					console.log(jsSnack.offsetLeft) 
 					jsSnack.style.top = jsSnack.offsetTop + 1 + "px";
 					jsSnack.style.left = jsSnack.offsetLeft + "px";
 					//记录小蛇的位置
@@ -83,10 +100,11 @@ document.onkeydown = function(e) {
 
  //食物随机出现
 function Pos(){
-    jsFood.css({"left":parseInt(Math.random() * (430 - 20 + 1) + 20) + "px"})
-    jsFood.css({"top":parseInt(Math.random() * (380 - 20 + 1) + 20) + "px"})
+    jsFood.style.left=parseInt(Math.random() * (playgroundWidth - jsFoodWidth ))/100 + "rem";
+    jsFood.style.top=parseInt(Math.random() * (playgroundheight - jsFoodWidth ))/100  + "rem";
 }
 Pos();
+
 
 function eat(){
     rectangleCrashExamine(jsSnack,jsFood);
@@ -106,24 +124,25 @@ function eat(){
             //碰撞后身体
             jsSnackBody = document.createElement("div");
             jsSnackBody.setAttribute("class","body");
-            jsBody.append(jsSnackBody);
+            jsBody.appendChild(jsSnackBody);
             Pos();//怪物的位置随机变换
-            setInterval(follow,10);//身体跟随的定时器
+          setInterval(follow,10);//身体跟随的定时器
         }
     }
 }
  function follow(){
     //检查一共添加了多少身体
-    var bodyNum = $(".body");
+    var bodyNum = document.getElementsByClassName("body");
+//  console.log(bodyNum)
     //记录得分
     jsScore.innerHTML = bodyNum.length;
     //蛇每次移动1个像素，那么新的身体应该跟随在当前数组的倒数第20个数组的位置;依次加等;
     var place = 0 ;
-  for( var i = 0 ; i<bodyNum.length ; i++){
-      place += 20;
-      console.log(num-place)
-    bodyNum[i].css({"left":srr[num-place][0] + 'px'})
-    bodyNum[i].css({"top":srr[num-place][1] + 'px'})    
-  }
+    for( var i = 0 ; i<bodyNum.length ; i++){
+//  	console.log(snackWidth)
+        place += snackWidth/2;
+//      bodyNum[i].style.left=srr[num-place][0] + 'px';
+//      bodyNum[i].style.top=srr[num-place][1] + 'px';    
+    }
 }
 
