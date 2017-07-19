bcefbd9c50c40cd3b58b25dfae1178637e4582fd
JKKE <2419308218@qq.com>
2017-07-20 00:03:55 +0800

    修改

--- lvxingzhe/js/app.js
+++ lvxingzhe/js/app.js
@@ -1,5 +1,6 @@
 (function(){//img自适应
 	var width=$(".head-bg").outerWidth();
+	var imgwidth2=$(".aobout-head-bg-img").outerWidth();
 	var windowwidth=$(window).width();
 	var height=$(window).height();
 	function size(){
@@ -8,6 +9,16 @@
 		$(".head").css({height:height})
 		$(".head-bg").css({transform:"translateX("+num1+"px)",height:height})
 		$(".bussiness-li img").css({transform:"translateX("+num1+"px)",height:height})
+		//关于我们的背景变化
+		$(".about-kong").css({height:height});
+		if(windowwidth<1025 && windowwidth>414){
+			var num2=-(imgwidth2-winwidth1)
+			$(".aobout-head-bg-img").css({transform:"translateX("+num2+"px)",height:height})
+		}
+		else{
+		var num2=-(imgwidth2-winwidth1)/2
+		}
+		$(".aobout-head-bg-img").css({transform:"translateX("+num2+"px)",height:height})
 		//设置第二块固定视图的高度
 		$(".jsheight").css({height:height})
 		//设置视频
@@ -16,9 +27,6 @@
 		var videoheight=$(".fixed-video-main").height();
 		$(".fixed-video-main").css({marginTop:-(videoheight/2)+"px"})
 		$(".fixed-video-main").css({marginLeft:-(videowidth/2)+"px"})
-		if(windowwidth<415){
-			$(".fixed-video-main").css({marginLeft:-(videowidth/1.7)+"px"})
-		}
 	};
 	size();	
 	$(window).resize(function(){//屏幕变化时候触发
@@ -29,6 +37,16 @@
 	$(".head-bg").css({transform:"translateX("+num+"px)",height:height2})
 	$(".bussiness-li img").css({transform:"translateX("+num+"px)",height:height2})
 	$(".fixed-video-main").css({height:height*0.3,width:windowwidth});
+	//关于我们的背景变化
+	$(".about-kong").css({height:height2});
+		if(winwidth<1025 && winwidth>414){
+			var num2=-(imgwidth2-winwidth)
+			$(".aobout-head-bg-img").css({transform:"translateX("+num2+"px)",height:height2})
+		}
+		else{
+		var num2=-(imgwidth2-winwidth)/2
+		$(".aobout-head-bg-img").css({transform:"translateX("+num2+"px)",height:height2})
+		}
 	//设置第二块固定视图的高度
 		$(".jsheight").css({height:height2})
 	//设置视频
@@ -37,9 +55,6 @@
 		var videoheight=$(".fixed-video-main").height();
 		$(".fixed-video-main").css({marginTop:-(videoheight/2)+"px"})
 		$(".fixed-video-main").css({marginLeft:-(videowidth/2)+"px"})
-		if(windowwidth<415){
-			$(".fixed-video-main").css({marginLeft:-(videowidth/1.7)+"px"})
-		}
 		if(winwidth>930){
 			$(".position-div").removeClass("myframesinit");
 			$(".position-div").removeClass("myframesout");
@@ -61,6 +76,7 @@
 })();
 var josn={
 	bool:false,
+	boolnum:1,
 	fadeOut:function(){//功能视频的淡入淡出
 		$(".index-middle-div-i").click(function(){
 			$(".fixed-video").fadeIn(400);
@@ -69,9 +85,30 @@ var josn={
 			$(".fixed-video").fadeOut(400);
 		})
 	},
+	Switch:function(){
+		var _this=this;
+		if(_this.boolnum==1){
+			$(".bussiness-li li").eq(0).fadeOut(500);
+			$(".bussiness-li li").eq(1).fadeIn(1000);
+			_this.boolnum=2;
+		}
+		else if(_this.boolnum==2){
+			$(".bussiness-li li").eq(1).fadeOut(500);
+			$(".bussiness-li li").eq(2).fadeIn(1000);
+			_this.boolnum=3;
+		}
+		else if(_this.boolnum==3){
+			$(".bussiness-li li").eq(2).fadeOut(500);
+			$(".bussiness-li li").eq(0).fadeIn(1000);
+			_this.boolnum=1;
+		}
+	},
 	init:function(){
 		var _this=this;
 		_this.fadeOut();
+		setInterval(function(){
+		    _this.Switch();
+		},3000);
 		$(".head-position-div").click(function(){//功能菜单栏出来和隐藏
 			if(_this.bool==false){
 				
